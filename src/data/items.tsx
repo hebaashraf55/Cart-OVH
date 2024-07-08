cdd79ce34e20852d5b2a031d6b467d3eaa184806
heba <hebaashraf81@gmail.com>
2024-07-09 00:12:12 +0300

    deploy-3

--- src/data/items.tsx
+++ src/data/items.tsx
@@ -12,7 +12,9 @@ export const items: Item[] = [
     name: "Item 1",
     description: "Description for item it will 1",
     price: 100,
-    image: ' https://img.freepik.com/free-psd/helmet-mock-up_1310-157.jpg?w=826&t=st=1720218999~exp=1720219599~hmac=c7739e47adc0ed865576f817a32cfb72abdb3f21354e5bf45477770467fad0bb'},
+    image:
+      " https://img.freepik.com/free-psd/helmet-mock-up_1310-157.jpg?w=826&t=st=1720218999~exp=1720219599~hmac=c7739e47adc0ed865576f817a32cfb72abdb3f21354e5bf45477770467fad0bb",
+  },
   {
     id: 2,
     name: "Item 2",
